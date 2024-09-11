6fd29b721791b9ab1a409f2732043eb4d2ca059e
Jaime Liew <jaimeliew1@gmail.com>
2024-09-11 16:52:03 +0200

    add modal verbs

--- src/german_anki_deck/cli.py
+++ src/german_anki_deck/cli.py
@@ -17,12 +17,13 @@ def make_deck():
 
     german_verbs = GermanVerbs.model_validate_json(data)
     verb_list = [verb.infinitive[1] for verb in german_verbs.verbs]
-    print(f"Generating Anki deck for the following {len(verb_list)} verbs:")
+    anki_notes = german_verbs.anki_notes()
+    print(f"Generating Anki deck ({len(anki_notes)} notes) for the following {len(verb_list)} verbs:")
     print(verb_list)
 
     my_deck = genanki.Deck(2059400110, "German verbs")
 
-    for note in german_verbs.anki_notes():
+    for note in anki_notes:
         my_deck.add_note(note)
 
     genanki.Package(my_deck).write_to_file("output.apkg")
